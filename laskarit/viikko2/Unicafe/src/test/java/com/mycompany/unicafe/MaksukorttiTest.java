7117dd474a518ed6f45979f26b4080a33d9de427
ansketom <anssi.ketomaki@helsinki.fi>
2020-03-24 17:10:28 +0200

    viikko2 hakemistorakenne talteen

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,35 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    @Test
+    public void kortinSaldoAlussaOikein(){
+        assertEquals(10, kortti.saldo());
+    }
+    @Test
+    public void rahanLataaminenKasvattaaSaldoa(){
+        kortti.lataaRahaa(15);
+        kortti.lataaRahaa(5);
+        assertEquals(30, kortti.saldo());
+    }
+    @Test
+    public void kortinSaldoVaheneeKunRahaaOtetaan(){
+        kortti.otaRahaa(5);
+        //saldo nyt 5
+        assertEquals(5, kortti.saldo());
+    }
+    @Test
+    public void kortinSaldoaEiVoiVahentaaMiinukselle(){
+        kortti.otaRahaa(5);
+        //kortin saldo nyt 5
+        kortti.otaRahaa(7);
+        assertEquals(5, kortti.saldo());
+    }
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaaRiittavasti(){
+        assertTrue(kortti.otaRahaa(5));
+    }
+    @Test
+    public void otaRahaaPalauttaFalseJosRahaaEiRiittavasti(){
+        assertFalse(kortti.otaRahaa(12));
+    }
 }
